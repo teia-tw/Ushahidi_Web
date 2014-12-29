aafceca4d2adb4fbf0b4e1dc2e1f0d807aa479f9
Pomin Wu <pomin5@gmail.com>
2014-12-29 19:57:34 +0800

    loosen person_(first|last) rules
    allow 1 character names

--- application/helpers/reports.php
+++ application/helpers/reports.php
@@ -117,7 +117,7 @@ class reports_Core {
 		// Validate Personal Information
 		if ( ! empty($post->person_first))
 		{
-			$post->add_rules('person_first', 'length[2,100]');
+			$post->add_rules('person_first', 'length[1,100]');
 		}
 		else
 		{
@@ -126,7 +126,7 @@ class reports_Core {
 
 		if ( ! empty($post->person_last))
 		{
-			$post->add_rules('person_last', 'length[2,100]');
+			$post->add_rules('person_last', 'length[1,100]');
 		}
 		else
 		{
